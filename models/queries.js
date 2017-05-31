878b6c17fc75bb685e9002745750d743fb7f074d
aaronev <aarone.villanueva@gmail.com>
2017-05-31 10:20:01 -0700

    updated the schema and deleted csv files

--- models/queries.js
+++ models/queries.js
@@ -2,32 +2,110 @@ const pgp = require('pg-promise')();
 const connectionString = 'postgres://localhost:5432/music';
 const db = pgp(connectionString);
 
+//Artists
+function getAllArtists() {
+  return db.any('SELECT * FROM artists;')
+}
+
+function getArtistsByID(id) {
+  return db.one(`SELECT * FROM artists WHERE id = ${id};`)
+}
+
+function getArtistsByName(name) {
+  return db.one (`SELECT * FROM artists WHERE name = ${name};`)
+}
+
+function addArtists(id, name, genre) {
+  return db.none(`INSERT INTO artists VALUES (${id}, ${name}, ${genre});`)
+}
+
+function updateArtists(id, name, genre) {
+  return db.none(`UPDATE artists SET name = ${name}, genre = ${genre};`)
+}
+
+function deleteArtists(id) {
+  return db.none(`DELETE FROM artists WHERE id = ${id}`)
+}
+
+//Albums
+function getAllAlbums() {
+  return db.any('SELECT * FROM Albums;')
+}
+
+function getAlbumsByID(id) {
+  return db.one(`SELECT * FROM Albums WHERE id = ${id};`)
+}
+
+function getAlbumsByName(name) {
+  return db.one (`SELECT * FROM Albums WHERE name = ${name};`)
+}
+
+function addAlbums(id, name, genre) {
+  return db.none(`INSERT INTO Albums VALUES (${id}, ${name}, ${genre});`)
+}
+
+function updateAlbums(id, name, genre) {
+  return db.none(`UPDATE Albums SET name = ${name}, genre = ${genre};`)
+}
+
+function deleteAlbums(id) {
+  return db.none(`DELETE FROM artists WHERE id = ${id}`)
+}
+
+//Songs
 function getAllSongs() {
-	return db.any('SELECT * FROM songs')
+  return db.any('SELECT * FROM Songs;')
 }
 
-function addSong(song, author, genre) {
-	return db.none(`INSERT INTO music(title, author, genre) VALUES(${title}, ${author}, ${genre})`)
+function getSongsByID(id) {
+  return db.one(`SELECT * FROM Songs WHERE id = ${id};`)
 }
 
-function updateSong(id, song, artist, genre, album) {
-	return  db.one(
-		`UPDATE books SET title = $1, author = $2, genre = $3 WHERE id = $4 returning *`, [song, artist, genre, bookId]
-		)
+function getSongsByName(name) {
+  return db.one (`SELECT * FROM Songs WHERE name = ${name};`)
 }
 
-function findSong(songId) {
-	return db.one('SELECT * FROM books WHERE id = $1', songId)
+function addSongs(id, name, genre) {
+  return db.none(`INSERT INTO Songs VALUES (${id}, ${name}, ${genre});`)
 }
 
-function deleteSong(songId) {
-	return db.none('DELETE FROM bookstore WHERE id = $1', songId)
+function updateSongs(id, name, genre) {
+  return db.none(`UPDATE Songs SET name = ${name}, genre = ${genre};`)
+}
+
+function deleteSongs(id) {
+  return db.none(`DELETE FROM artists WHERE id = ${id}`)
+}
+
+//Playlists
+function getAllPlaylists() {
+  return db.any('SELECT * FROM playlists;')
+}
+
+function getSongsByID(id) {
+  return db.one(`SELECT * FROM Songs WHERE id = ${id};`)
+}
+
+function getSongsByName(name) {
+  return db.one (`SELECT * FROM Songs WHERE name = ${name};`)
+}
+
+function addSongs(id, name, genre) {
+  return db.none(`INSERT INTO Songs VALUES (${id}, ${name}, ${genre});`)
+}
+
+function updateSongs(id, name, genre) {
+  return db.none(`UPDATE Songs SET name = ${name}, genre = ${genre};`)
+}
+
+function deleteSongs(id) {
+  return db.none(`DELETE FROM artists WHERE id = ${id}`)
 }
 
 module.exports = {
-	getAllSongs,
-	addSong,
-	updateSong,
-	findSong,
-	deleteSong
+  getAllSongs,
+  addSong,
+  updateSong,
+  findSong,
+  deleteSong
 };
--- models/schema.sql
+++ models/schema.sql
@@ -1,42 +1,75 @@
+-- Note: I did not work on the INIT 4 GOAL, which was required. I had created  
+
 DROP TABLE IF EXISTS artists;
 CREATE TABLE artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(60),
+  genre VARCHAR(60)
 );
 
--- note: paths differ depending on user
-COPY artists FROM '/Users/aaronvillanueva/Desktop/api-prac/models/artists.csv' DELIMITER ',';
+INSERT INTO artists (name, genre) VALUES ( 'Paul', 'Nania');
+INSERT INTO artists (name, genre) VALUES ( 'Some Artists', 'A Cool Genre');
+INSERT INTO artists (name, genre) VALUES ( 'Another Artists', 'Cooler Genre');
+INSERT INTO artists (name, genre) VALUES ( 'Snoop Dog', 'R&B');
 
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-  id INTEGER PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   artist_id INTEGER REFERENCES artists,
-  title TEXT,
+  title VARCHAR(60),
   year INTEGER
 );
 
-COPY albums FROM '/Users/aaronvillanueva/Desktop/api-prac/models/albums.csv' DELIMITER ',';
+INSERT INTO albums (artist_id, title, year) VALUES (1, 'Brown Sugar', 1995);
+INSERT INTO albums (artist_id, title, year) VALUES (1, 'Voodoo', 2000);
+INSERT INTO albums (artist_id, title, year) VALUES (2, 'House with No Home', 2008);
+INSERT INTO albums (artist_id, title, year) VALUES (2, 'Another Album', 2002);
+INSERT INTO albums (artist_id, title, year) VALUES (3, 'The Coolest Album', 1995);
 
 DROP TABLE IF EXISTS songs;
 CREATE TABLE songs (
-  title TEXT,
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(60),
   album_id INTEGER REFERENCES albums,
   length_sec INTEGER,
   track_no INTEGER
 );
 
-COPY songs FROM '/Users/aaronvillanueva/Desktop/api-prac/models/songs.csv' DELIMITER ',';
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Playa Playa', 1, 426, 1);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Devils Pie', 1, 321, 2);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Left & Right', 1, 286, 3);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Brown Sugar', 2, 263, 1);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Alright', 2, 313, 2);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Jonz In My Bonz', 2, 356, 3);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Rude to Rile', 3, 209, 2);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Working Poor', 3, 196, 3);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Lost Ones', 4, 333, 2);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Ex Factor', 4, 326, 3);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('To Zion', 4, 369, 4);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Bird Flu', 5, 204, 2);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Boyz', 5, 207, 3);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Jimmy', 5, 209, 4);
+INSERT INTO songs (title, album_id, length_sec, track_no) VALUES ('Hussel', 5, 265, 5);
 
 CREATE TABLE playlists (
-  title TEXT,
-  album_id INTEGER REFERENCES albums,
-  artist_id INTEGER REFERENCES artists
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(60),
+  song_id INTEGER REFERENCES songs
 );
 
-COPY playlists FROM '/Users/aaronvillanueva/Desktop/api-prac/models/playlists.csv' DELIMITER ',';
+INSERT INTO playlists (title, song_id) VALUES ('Dance Jams', 1); 
+INSERT INTO playlists (title, song_id) VALUES ('Dance Jams', 2); 
+INSERT INTO playlists (title, song_id) VALUES ('Dance Jams', 3); 
+INSERT INTO playlists (title, song_id) VALUES ('Dance Jams', 4); 
+INSERT INTO playlists (title, song_id) VALUES ('Dance Jams', 5); 
+INSERT INTO playlists (title, song_id) VALUES ('Dance Jams', 6); 
+INSERT INTO playlists (title, song_id) VALUES ('Romantic Nitez', 7);
+INSERT INTO playlists (title, song_id) VALUES ('Romantic Nitez', 8);
+INSERT INTO playlists (title, song_id) VALUES ('Romantic Nitez', 9);
+INSERT INTO playlists (title, song_id) VALUES ('Romantic Nitez', 10);
+INSERT INTO playlists (title, song_id) VALUES ('Romantic Nitez', 11);
+INSERT INTO playlists (title, song_id) VALUES ('Romantic Nitez', 12);
+INSERT INTO playlists (title, song_id) VALUES ('Spacey', 13); 
+INSERT INTO playlists (title, song_id) VALUES ('Spacey', 14); 
+INSERT INTO playlists (title, song_id) VALUES ('Spacey', 15); 
 
-SELECT * FROM songs 
-JOIN albums ON songs.album_id = albums.id
-JOIN artists ON albums.artist_id = artists.id
-JOIN playlists ON albums.id = playlists.album_id
